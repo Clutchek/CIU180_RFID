221581e21699a947fad8b753e209450ab28ecc3d
clutchek <nalex.k@hotmail.com>
2018-03-01 14:18:00 +0100

    Changing ports and LEDs

--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -4,21 +4,38 @@
 // Compatible ports 10, 11, 12, 13, 14, 15, 50, 51, 52, 53, A8 (62), A9 (63), A10 (64), A11 (65), A12 (66), A13 (67), A14 (68), A15 (69)
  
 
-SoftwareSerial card1(12, 13);
-SoftwareSerial card3(50, 51);
-//SoftwareSerial Serial3(14,15);
-//SoftwareSerial Serial2(8,9);
-//SoftwareSerial comp(10,11);
 
-//LEDS
-int card2LED = 7;
-int card3LED = 53;
+//Card1 - Serial3 (15)
+//Card2 - Serial2
+//Card3 - Serial1
+
+#define card1 Serial3
+#define card2 Serial2
+#define card3 Serial1
+
+//The rfid that confirms the solution is using the softwareserial library
+SoftwareSerial reader(50, 51);
 
 //RGB -  Card 1
-int rgbRed = 9;
-int rgbGreen = 11;
-int rgbBlue = 10;
+int rgb1Red = 9;
+int rgb1Green = 11;
+int rgb1Blue = 10;
 
+//RGB - Card 2
+int rgb2Red = 6;
+int rgb2Green = 5;
+int rgb2Blue = 4;
+
+//RGB - Card 3
+int rgb3Red = 36;
+int rgb3Green = 37;
+int rgb3Blue = 39;
+
+//RGB - Reader
+
+int readerRed = 49;
+int readerGreen = 48;
+int readerBlue = 53;
  
 String part1, part2, part3;
 int chosenSol;
@@ -30,44 +47,63 @@ String chosenTag;
 String tagString;
 char tagNumber[14];
 
-unsigned char buffer[64];       // buffer array for data receive over serial port
-int count = 0;                    // counter for buffer array
+//unsigned char buffer[64];       // buffer array for data receive over serial port
+//int count = 0;                    // counter for buffer array
  
-
 String solutions[4][3];
+
+int delayTime = 1000;
  
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(rgbGreen, OUTPUT);
-  pinMode(rgbRed, OUTPUT);
-  pinMode(rgbBlue, OUTPUT);
-  digitalWrite(rgbRed, 0);
-  digitalWrite(rgbGreen, 0);
-  digitalWrite(rgbBlue, 0);
+  //start the lights and make sure they are not displaying any colors
+  pinMode(rgb1Green, OUTPUT);
+  pinMode(rgb1Red, OUTPUT);
+  pinMode(rgb1Blue, OUTPUT);
+  digitalWrite(rgb1Red, 0);
+  digitalWrite(rgb1Green, 0);
+  digitalWrite(rgb1Blue, 0);
 
-  pinMode(card2LED, OUTPUT);
-  pinMode(card3LED, OUTPUT);
+  pinMode(rgb2Green, OUTPUT);
+  pinMode(rgb2Red, OUTPUT);
+  pinMode(rgb2Blue, OUTPUT);
+  digitalWrite(rgb1Red, 0);
+  digitalWrite(rgb1Green, 0);
+  digitalWrite(rgb1Blue, 0);
 
+  pinMode(rgb3Green, OUTPUT);
+  pinMode(rgb3Red, OUTPUT);
+  pinMode(rgb3Blue, OUTPUT);
+  digitalWrite(rgb3Red, 0);
+  digitalWrite(rgb3Green, 0);
+  digitalWrite(rgb3Blue, 0);
   
+  pinMode(readerGreen, OUTPUT);
+  pinMode(readerRed, OUTPUT);
+  pinMode(readerBlue, OUTPUT);
+  digitalWrite(readerRed, 0);
+  digitalWrite(readerGreen, 0);
+  digitalWrite(readerBlue, 0);
   
+  //Start the connection to the computer
   Serial.begin(9600);
   while(!Serial){
     ;
   }
+  //Start the rfid readers
+  Serial3.begin(9600);
   Serial2.begin(9600);
   Serial1.begin(9600);
-  
-  card1.begin(9600);
-  Serial3.begin(9600);
-  card3.begin(9600);
-  
-  
- 
-  //Serial.print(sol);
+  reader.begin(9600);
 
+  reader.listen();
+  
   //initiating chosen solution to 1 as default
   chosenSol = 1;
+
+  //The tag number of the tag that you want to "program"
+  chosenTag = "410042E50AEC";
 
   //Solutions
   solutions[1][1] = "410043C377B6";
@@ -75,21 +111,12 @@ void setup() {
   solutions[1][3] = "410043C377B6";
   //Tag Number: 410043C377B6
 
-
-  
 }
  
 void loop() {
   // put your main code here, to run repeatedly:
   
   //delay(500);
-
-  /*if(Serial.available()  > 0){
-    char newSol = Serial.read();  
-    if(newSol != chosenSol){
-        
-    }
-  }*/
 
   //enabling new solution from computer
   /*if(Serial.available() > 0){
@@ -98,25 +125,7 @@ void loop() {
       chosenSol = newSol;  
     }
   }*/
-
-  //Read and store what tag you want to program
-  /*if(Serial2.available() > 0){
-    while(Serial2.available()){
-      chosenTag = Serial2.readBytesUntil(3, tagNumber, 15);//EOT (3) is the last character in tag
-    }
-    
-  }else{
-    //turn off all lamps
-    
-    digitalWrite(rgbRed, 0);
-    digitalWrite(rgbGreen, 0);
-    digitalWrite(rgbBlue, 0);
-    
-    digitalWrite(card2LED, LOW);
-    digitalWrite(card3LED, LOW);
-  }*/
-
-  card1.listen();
+  
   if(card1.available() > 0){
     while (card1.available()){
       int ReadBytes = card1.readBytesUntil(3, tagNumber, 15);//EOT (3) is the last character in tag
@@ -124,49 +133,41 @@ void loop() {
     }
     Serial.println("Tag is: " + part1);
     if(part1.equals(solutions[chosenSol][1])){
-      //start pink lamp
-      digitalWrite(rgbRed, 170);
-      digitalWrite(rgbGreen, 0);
-      digitalWrite(rgbBlue, 255);
+      //Solution found, turning on green
+      digitalWrite(rgb1Red, 0);
+      digitalWrite(rgb1Green, 255);
+      digitalWrite(rgb1Blue, 0);
       
     }else{
-      //turn off
-      digitalWrite(rgbRed, 0);
-      digitalWrite(rgbGreen, 0);
-      digitalWrite(rgbBlue, 0);
-      
+      //Solution not found, turning on red
+      digitalWrite(rgb1Red, 255);
+      digitalWrite(rgb1Green, 0);
+      digitalWrite(rgb1Blue, 0);
     }
-  }/*else{
-    //turn off lamp
-    
-      //digitalWrite(rgbRed, 0);
-      //digitalWrite(rgbGreen, 0);
-      //digitalWrite(rgbBlue, 0);
-  }*/
+  }
   
-  if(Serial3.available() > 0){
-    while (Serial3.available()){
-      int ReadBytes = Serial3.readBytesUntil(3, tagNumber, 15);//EOT (3) is the last character in tag
+  if(card2.available() > 0){
+    while (card2.available()){
+      int ReadBytes = card2.readBytesUntil(3, tagNumber, 15);//EOT (3) is the last character in tag
     }
-    
     part2 = tagNumber;
     Serial.println("Tag is: " + part2);
     
     
     if(part2.equals(solutions[chosenSol][2])){
-      //start blue led
-      digitalWrite(card2LED, HIGH);
-      Serial.print(" was not equal");
+      //Solution found, turning on green
+      digitalWrite(rgb2Red, 0);
+      digitalWrite(rgb2Green, 255);
+      digitalWrite(rgb2Blue, 0);
     }else{
-      //turn off
-      digitalWrite(card2LED, LOW);
+      //Solution not found, turning on red
+      digitalWrite(rgb2Red, 255);
+      digitalWrite(rgb2Green, 0);
+      digitalWrite(rgb2Blue, 0);
+      
     }
-  }else{
-    //turn off lamp
-    //digitalWrite(card3LED, LOW);
   }
   
-  //card3.listen();
   if(card3.available() > 0){
     Serial.println("Card 3 was avaiable");
     while (card3.available()){
@@ -178,45 +179,48 @@ void loop() {
     //clearBufferArray();    
 
     if(part3.equals(solutions[chosenSol][3])){
-      //start green lamp
-      digitalWrite(card3LED, HIGH);
-      //Serial.print("was equal");
+      //Solution found, turning on green
+      digitalWrite(rgb3Red, 0);
+      digitalWrite(rgb3Green, 255);
+      digitalWrite(rgb3Blue, 0);
+      
     }else{
-      //turn off
-      digitalWrite(card3LED, LOW);
-      //Serial.print("was not equal");
+      //Solution not found, turning on red
+      digitalWrite(rgb3Red, 255);
+      digitalWrite(rgb3Green, 0);
+      digitalWrite(rgb3Blue, 0);
     }
-  }else{
-    //turn off lamp
-    //digitalWrite(card2LED, LOW);
   }
   
-  if(Serial1.available() > 0){
+  if(reader.available() > 0){
     tagString = "";
-    while (Serial1.available()){
-      int ReadBytes = Serial1.readBytesUntil(3, tagNumber, 15);//EOT (3) is the last character in tag
-      tagString = tagNumber;
+    while (reader.available()){
+      int ReadBytes = reader.readBytesUntil(3, tagNumber, 15);//EOT (3) is the last character in tag
     }
+    tagString = tagNumber;
+    Serial.println(tagString);
 
     if(tagString.equals(chosenTag)){
       if(part1.equals(solutions[chosenSol][1]) && part2.equals(solutions[chosenSol][2]) && part3.equals(solutions[chosenSol][3])){
         //print victory over serial
         Serial.print(5);
+        digitalWrite(readerRed, 0);
+        digitalWrite(readerGreen, 255);
+        digitalWrite(readerBlue, 0);
+        
       }else{
         //print fail over serial
         Serial.print(6);
+        digitalWrite(readerRed, 255);
+        digitalWrite(readerGreen, 0);
+        digitalWrite(readerBlue, 0);
       }
+    }else{
+        //wrong tag scanned
+        digitalWrite(readerRed, 255);
+        digitalWrite(readerGreen, 0);
+        digitalWrite(readerBlue, 0);
     }
   }
-}
-
-void clearBufferArray()                 // function to clear buffer array
-{
-    // clear all index of array with command NULL
-    for (int i=0; i<count; i++)
-    {
-        buffer[i]=NULL;
-    }                  
-    count = 0;
 }
 
